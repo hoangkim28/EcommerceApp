03a5bef50d7602eae88ee76c24d1910c8ceee6cc
Kim Thanh Hoang <73277396+hoangkim28@users.noreply.github.com>
2021-07-26 22:22:14 +0700

    admin voyager custom css

--- config/voyager.php
+++ config/voyager.php
@@ -187,7 +187,7 @@ return [
 
     // Here you can specify additional assets you would like to be included in the master.blade
     'additional_css' => [
-        //'css/custom.css',
+        'css/voyager-custome-css.css',
     ],
 
     'additional_js' => [
