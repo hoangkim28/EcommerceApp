d00a8f123b0ef8d1e181c006d034022e635379d8
Kim Thanh Hoang <73277396+hoangkim28@users.noreply.github.com>
2021-08-16 08:26:37 +0700

    login livewire

---
+++ resources/views/layouts/nav.blade.php
@@ -0,0 +1,26 @@
+<header class="border-b border-palette-lighter sticky top-0 z-20 bg-white">
+  <div class="flex items-center justify-between mx-auto max-w-6xl px-6 pb-2 pt-4 md:pt-6"><a class=" cursor-pointer"
+      href="/">
+      <h1 class="flex no-underline">
+        <img height="32" width="32" alt="logo" 
+          class="h-8 w-8 mr-1 object-contain"
+          src="/icon.svg">
+      <span class="text-xl font-primary font-bold tracking-tight pt-1">
+        {{setting('site.title')}}</span>
+    </h1>
+    </a>
+    <div>
+      <a class=" relative" aria-label="cart" href="/cart"><svg aria-hidden="true" focusable="false" data-prefix="fas"
+          data-icon="shopping-cart" class="svg-inline--fa fa-shopping-cart fa-w-18 text-palette-primary w-6 m-auto"
+          role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+          <path fill="currentColor"
+            d="M528.12 301.319l47.273-208C578.806 78.301 567.391 64 551.99 64H159.208l-9.166-44.81C147.758 8.021 137.93 0 126.529 0H24C10.745 0 0 10.745 0 24v16c0 13.255 10.745 24 24 24h69.883l70.248 343.435C147.325 417.1 136 435.222 136 456c0 30.928 25.072 56 56 56s56-25.072 56-56c0-15.674-6.447-29.835-16.824-40h209.647C430.447 426.165 424 440.326 424 456c0 30.928 25.072 56 56 56s56-25.072 56-56c0-22.172-12.888-41.332-31.579-50.405l5.517-24.276c3.413-15.018-8.002-29.319-23.403-29.319H218.117l-6.545-32h293.145c11.206 0 20.92-7.754 23.403-18.681z">
+          </path>
+        </svg>
+        <div
+          class="absolute top-0 right-0 text-xs bg-yellow-300 text-gray-900 font-semibold rounded-full py-1 px-2 transform translate-x-10 -translate-y-3">
+          1</div>
+      </a>
+    </div>
+  </div>
+</header>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\Auth\EmailVerificationController;
+use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Livewire\Auth\Login;
+use App\Http\Livewire\Auth\Passwords\Confirm;
+use App\Http\Livewire\Auth\Passwords\Email;
+use App\Http\Livewire\Auth\Passwords\Reset;
+use App\Http\Livewire\Auth\Register;
+use App\Http\Livewire\Auth\Verify;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Admin\ProductSkuController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,20 +21,46 @@ use App\Http\Controllers\Admin\ProductSkuController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::view('/', 'welcome')->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home2');
+
+Route::middleware('guest')->group(function () {
+    Route::get('login', Login::class)
+        ->name('login');
+
+    Route::get('register', Register::class)
+        ->name('register');
 });
 
-Auth::routes();
+Route::get('password/reset', Email::class)
+    ->name('password.request');
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('password/reset/{token}', Reset::class)
+    ->name('password.reset');
 
+Route::middleware('auth')->group(function () {
+    Route::get('email/verify', Verify::class)
+        ->middleware('throttle:6,1')
+        ->name('verification.notice');
+
+    Route::get('password/confirm', Confirm::class)
+        ->name('password.confirm');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('email/verify/{id}/{hash}', EmailVerificationController::class)
+        ->middleware('signed')
+        ->name('verification.verify');
+
+    Route::post('logout', LogoutController::class)
+        ->name('logout');
+});
 
 Route::group(['prefix' => 'admin/'], function () {
-    Voyager::routes();
-    Route::get('products/{id}/sku', [ProductSkuController::class, 'show'])->name('product.productsku');
-    Route::post('products/sku/create', [ProductSkuController::class, 'store'])->name('product.productsku.store');
-    Route::put('products/sku/get/{id}', [ProductSkuController::class, 'get_sku'])->name('product.productsku.getsku');
-    Route::post('products/sku/delete', [ProductSkuController::class, 'delete_sku'])->name('product.productsku.deletesku');
+  Voyager::routes();
+  Route::get('products/{id}/sku', [ProductSkuController::class, 'show'])->name('product.productsku');
+  Route::post('products/sku/create', [ProductSkuController::class, 'store'])->name('product.productsku.store');
+  Route::put('products/sku/get/{id}', [ProductSkuController::class, 'get_sku'])->name('product.productsku.getsku');
+  Route::post('products/sku/delete', [ProductSkuController::class, 'delete_sku'])->name('product.productsku.deletesku');
 
 });
