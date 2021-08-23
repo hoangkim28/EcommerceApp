e72f67dfca0778d33e7711c3e46470d75f0ef1a5
Kim Thanh Hoang <73277396+hoangkim28@users.noreply.github.com>
2021-08-23 19:02:34 +0700

    cart delete item

---
+++ app/Http/Livewire/CartDeleteItem.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class CartDeleteItem extends Component
+{
+    public $rowId = [];
+
+    protected $listeners = [
+      'confirmed_delete'
+  ];
+
+    public function render()
+    {
+        return view('livewire.cart-delete-item');
+    }
+
+    public function mount($rowId)
+    {
+      $this->rowId = $rowId;
+    }
+
+    public function remove()
+    {
+      $cartItem = \Cart::get($this->rowId);
+      $this->confirm('Xác nhận xóa '.$cartItem->name, [
+        'toast' => false,
+        'position' => 'center',
+        'showConfirmButton' => true,
+        'cancelButtonText' => 'Nope',
+        'onConfirmed' => 'confirmed_delete',
+        'onCancelled' => 'cancelled'
+      ]);
+    }
+    public function confirmed_delete()
+    {      
+      \Cart::remove($this->rowId);
+      $this->emit('cartUpdated');
+      $this->emit('updateHeaderCartCount');
+      $this->emit('updateCartContent');
+    }
+}
