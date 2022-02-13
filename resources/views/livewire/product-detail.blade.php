003099d1b60998be92ec5809b480a9e321e7f721
Kim Thanh Hoang <hoangkim28@outlook.com>
2022-02-13 19:42:08 +0700

    button addtocart - checking quantity

--- resources/views/livewire/product-detail.blade.php
+++ resources/views/livewire/product-detail.blade.php
@@ -121,8 +121,8 @@
         </div>
         <div class="w-full">
           <div class="flex flex-row space-x-1 md:space-x-6">
-            <a wire:click="addCart"
-              class="cursor-pointer pt-3 pb-2 bg-red-500 text-white w-full md:w-1/2 mt-2 rounded-sm font-primary font-semibold text-xl flex justify-center items-center  hover:bg-black"
+            <button wire:click="addCart" @if($quantity === 0) disabled @endif
+              class="cursor-pointer pt-3 pb-2 bg-red-500 text-white w-full md:w-1/2 mt-2 rounded-sm font-primary font-semibold text-xl flex justify-center items-center hover:bg-black @if($quantity === 0) cursor-not-allowed @else cursor-pointer hover:bg-black hover:border-black @endif"
               aria-label="cart-button">
               Thêm giỏ hàng
               <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="shopping-cart"
@@ -132,7 +132,7 @@
                   d="M528.12 301.319l47.273-208C578.806 78.301 567.391 64 551.99 64H159.208l-9.166-44.81C147.758 8.021 137.93 0 126.529 0H24C10.745 0 0 10.745 0 24v16c0 13.255 10.745 24 24 24h69.883l70.248 343.435C147.325 417.1 136 435.222 136 456c0 30.928 25.072 56 56 56s56-25.072 56-56c0-15.674-6.447-29.835-16.824-40h209.647C430.447 426.165 424 440.326 424 456c0 30.928 25.072 56 56 56s56-25.072 56-56c0-22.172-12.888-41.332-31.579-50.405l5.517-24.276c3.413-15.018-8.002-29.319-23.403-29.319H218.117l-6.545-32h293.145c11.206 0 20.92-7.754 23.403-18.681z">
                 </path>
               </svg>
-            </a>
+            </button>
           </div>
 
             <div wire:loading wire:target="addCart" class="text-black animate-pulse py-2 text-red-500 text-lg">
